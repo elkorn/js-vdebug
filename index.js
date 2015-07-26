838a9e2a073cba83be8e8be1961d244c7c965421
Korneliusz Caputa <helluinster@gmail.com>
2015-07-26 20:59:09 +0200

    Handle input paths from command line.

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ import getReferenceHandlers from './getReferenceHandlers';
 import traverse from './traverse';
 import fs from 'fs';
 
+import log from './log';
+
+let input;
+if(process.argv[2]) {
+  input = fs.readFileSync(process.argv[2]);
+}
+
 traverse({
+  input: input,
   customHandlerGroups: [
     getReferenceHandlers
   ],
   done: ({
     ast, result
-  }) => fs.writeFile('syntax.json', JSON.stringify(result, null, '  '))
+  }) => {
+    fs.writeFile('syntax.json', JSON.stringify(result, null, '  '));
+  }
 });
