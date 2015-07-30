0121c37092c6cb35a12c2353ddd5afa2d402768e
Korneliusz Caputa <helluinster@gmail.com>
2015-07-30 22:14:17 +0200

    Don't initialize stuff when not needed.

--- traverse.js
+++ traverse.js
@@ -30,26 +30,26 @@ default
 function({
   customHandlerGroups, done, input
 }) {
-  let scopeChain = new ScopeChain();
-  let result = [];
-  const addToResultHandler = {
-    leave: {
-      always: inCurrentScope(({
-        scope
-      }) => {
-        if (result.indexOf(scope) === -1) {
-          result.unshift(scope);
-        }
-      })
-    }
-  };
-
-  const handlerGroups = [scopeHandlers, addToResultHandler, variableHandlers, ...customHandlerGroups];
-
-  const enterHandlerGroups = composeHandlerGroups(handlerGroups, 'enter');
-  const leaveHandlerGroups = composeHandlerGroups(handlerGroups.reverse(), 'leave');
-
   withAST(input, function(ast) {
+    let scopeChain = new ScopeChain();
+    let result = [];
+    const addToResultHandler = {
+      leave: {
+        always: inCurrentScope(({
+          scope
+        }) => {
+          if (result.indexOf(scope) === -1) {
+            result.unshift(scope);
+          }
+        })
+      }
+    };
+
+    const handlerGroups = [scopeHandlers, addToResultHandler, variableHandlers, ...customHandlerGroups];
+
+    const enterHandlerGroups = composeHandlerGroups(handlerGroups, 'enter');
+    const leaveHandlerGroups = composeHandlerGroups(handlerGroups.reverse(), 'leave');
+
     estraverse.traverse(ast, {
       enter: runHandlers(enterHandlerGroups, scopeChain),
       leave: runHandlers(leaveHandlerGroups, scopeChain)
